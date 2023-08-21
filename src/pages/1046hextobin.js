4a42568c9063ddcea4dceed151759b1f70805564
IgorKha <hahabahov@yandex.ru>
2023-08-21 18:35:14 +0500

    add ls1046a rcw tool

---
+++ src/pages/1046hextobin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Rcw1046 from '../components/Ls1046RCW/index';
+
+function Home() {
+  return (
+    <Layout>
+      <main>
+        <Rcw1046 />
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
